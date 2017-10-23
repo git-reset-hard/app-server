d29ceba5bcb6d44c70da0b7d8be6c64c1d2f56ba
DanDai <ddai85@gmail.com>
2017-10-23 12:34:00 -0700

    Make initial server get request handler and write a few tests to check functionality. Start implementing MySQL database.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,47 @@
-const restaurantList = require('./database/restaurantdb.js');
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const bodyParser = require('body-parser');
+const request = require('request');
+const port = process.env.PORT || 2424;
+//const restaurantList = require('./database/restaurantdb.js');
+const appServerDB = require('./database/mysql.js');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.get('/', (req, res) => {
+  res.status(200);
+  res.send('Serving up webpage');
+});
+
+//GET handler will use req.query.OBJ to get query string, location and userId of query
+app.get('/searchRestaurants', (req, res) => {
+  //check to make sure query parameters are valid
+  if (req.query.searchTerm || req.query.location || req.query.userId) {
+    //check userId against database to see if generic list or personalized list will be served
+    //if generic-- query elasticsearch restaurant list and come up with list
+    //else MAKE POST request to recommendations engine
+
+    //return list of restaurants
+    res.status(200);
+    res.send('Sending back list');
+
+    //store query to database along with list
+    //send to analytics for storage
+  } else {
+    res.status(400);
+    res.send('search parameters cannot be undefined');
+  }
+});
+
+
+
+
+
+
 
 // restaurantList.search({
 //   index: 'restaurant',
@@ -10,3 +53,9 @@ const restaurantList = require('./database/restaurantdb.js');
 //   .catch((err) => {
 //     console.log('query error ', error);
 //   }); 
+
+
+
+server.listen(port, () => {
+  console.log(`(>^.^)> Server now listening on ${port}!`);
+});
